646f159c986c3b38cce33b18c1101a8cbf9b7cb5
adith-widhiantara <54925429+adith-widhiantara@users.noreply.github.com>
2020-12-21 18:57:36 +0700

    Add Bootstrap

---
+++ resources/views/component/navbar.blade.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Mir'a Collection</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            Kategori
+          </a>
+        </li>
+      </ul>
+
+      <div class="login-right my-2 my-lg-0">
+        <a class="pr-3" href="#">
+          <i class="fas fa-shopping-cart pr-1"></i>
+          <span>Keranjang</span>
+        </a>
+        <a class="pl-3" href="#">
+          <i class="fas fa-user pr-1"></i>
+          <span>Masuk</span>
+        </a>
+      </div>
+    </div>
+  </div>
+</nav>
